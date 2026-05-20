9467b51e099ce80eb6105f5000ac77866dd66533
limitless <limitless0163@gmail.com>
2026-05-20 18:39:03 +0800

    a commit

---
+++ 2_deep-learning-computation/test05.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+
+print(torch.device('cpu'), torch.device('cuda'), torch.device('cuda:1'))
+
+print(torch.cuda.device_count())
+
+def try_gpu(i=0):
+    """如果存在，则返回gpu(i)，否则返回cpu()"""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+def try_all_gpus():
+    """返回所有可用的GPU，如果没有GPU，则返回[cpu(),]"""
+    devices = [torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count())] 
+    return devices if devices else [torch.device('cpu')]
+
+print(try_gpu(), try_gpu(10), try_all_gpus())
+
+
