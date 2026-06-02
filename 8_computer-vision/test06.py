d618445c73cf9bf6d76d7c9a88d6984a123a8351
limitless <limitless0163@gmail.com>
2026-06-02 21:57:28 +0800

    ImageNet-Dogs

--- 8_computer-vision/test06.py
+++ 8_computer-vision/test06.py
@@ -1,8 +1,10 @@
 import os
+import pandas as pd
 import torch
 import torchvision
 from torch import nn
 from d2l import torch as d2l
+from tqdm import tqdm
 
 data_dir = os.path.join('..', 'data', 'dog-breed-identification')
 
@@ -56,7 +58,7 @@ loss = nn.CrossEntropyLoss(reduction='none')
 
 def evaluate_loss(data_iter, net, devices):
     l_sum, n = 0.0, 0
-    for features, labels in data_iter:
+    for features, labels in tqdm(data_iter, desc='评估损失', unit='batch', leave=False):
         features, labels = features.to(devices[0]), labels.to(devices[0])
         outputs = net(features)
         l = loss(outputs, labels)
@@ -74,9 +76,11 @@ def train(net, train_iter, valid_iter, num_epochs, lr, wd, devices, lr_period, l
     if valid_iter is not None:
         legend.append('valid loss')
     animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs], legend=legend)
-    for epoch in range(num_epochs):
+    pbar_epoch = tqdm(range(num_epochs), desc='Training', unit='epoch')
+    for epoch in pbar_epoch:
         metric = d2l.Accumulator(2)
-        for i, (features, labels) in enumerate(train_iter):
+        pbar_batch = tqdm(enumerate(train_iter), total=num_batches, desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch', leave=False)
+        for i, (features, labels) in pbar_batch:
             timer.start()
             features, labels = features.to(devices[0]), labels.to(devices[0])
             trainer.zero_grad()
@@ -86,12 +90,15 @@ def train(net, train_iter, valid_iter, num_epochs, lr, wd, devices, lr_period, l
             trainer.step()
             metric.add(l, labels.shape[0])
             timer.stop()
+            train_loss = metric[0] / metric[1]
+            pbar_batch.set_postfix({'loss': f'{train_loss:.3f}'})
             if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
-                animator.add(epoch + (i + 1) / num_batches, (metric[0] / metric[1], None))
+                animator.add(epoch + (i + 1) / num_batches, (train_loss, None))
         measures = f'train loss {metric[0] / metric[1]:.3f}'
         if valid_iter is not None:
             valid_loss = evaluate_loss(valid_iter, net, devices)
             animator.add(epoch + 1, (None, valid_loss.detach().cpu()))
+        pbar_epoch.set_postfix({'train_loss': f'{metric[0] / metric[1]:.3f}', 'valid_loss': f'{valid_loss:.3f}' if valid_iter is not None else 'N/A'})
         scheduler.step()
     if valid_iter is not None:
         measures += f', valid loss {valid_loss:.3f}'
@@ -106,14 +113,22 @@ net = get_net(devices)
 train(net, train_valid_iter, None, num_epochs, lr, wd, devices, lr_period, lr_decay)
 
 preds = []
-for data, label in test_iter:
-    output = torch.nn.functional.softmax(net(data.to(devices[0])), dim=1)
-    preds.extend(output.cpu().detach().numpy())
+net.eval()
+with torch.no_grad():
+    for data, label in tqdm(test_iter, desc='测试集推理', unit='batch'):
+        output = torch.nn.functional.softmax(net(data.to(devices[0])), dim=1)
+        preds.extend(output.cpu().numpy())
+
 ids = sorted(os.listdir(os.path.join(data_dir, 'train_valid_test', 'test', 'unknown')))
-with open('submission.csv', 'w') as f:
-    f.write('id,' + ','.join(train_valid_ds.classes) + '\n')
-    for i, output in zip(ids, preds):
-        f.write(i.split('.')[0] + ',' + ','.join([str(num) for num in output]) + '\n')
+ids = [i.split('.')[0] for i in ids]
+
+df = pd.DataFrame(preds, columns=train_valid_ds.classes)
+df.insert(0, 'id', ids)
+
+output_path = 'submission.csv'
+print('正在写入 submission.csv ...')
+df.to_csv(output_path, index=False)
+print(f'已保存到 {output_path}')
 d2l.plt.show()
 
 
